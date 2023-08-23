3ffbc64cce90749a57207fdf201ccc6381750673
欧阳国军 <5135140666@qq.com>
2023-08-23 14:56:04 +0800

    添加库属性项目

---
+++ 008LibraryProperty/src/linear.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "linear.h"
+
+void linear_fit()
+{
+    std::cout<<"linear_fit called"<<std::endl;
+}
---
+++ 008LibraryProperty/src/linear.h
@@ -0,0 +1,6 @@
+#ifndef __LINEAR_H_
+#define __LINEAR_H_
+
+__declspec(dllexport) void linear_fit();
+
+#endif
